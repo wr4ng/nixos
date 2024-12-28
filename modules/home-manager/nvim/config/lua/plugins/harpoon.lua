71ceb2e13f2729451d8fd0fb76379e0e056393e5
Mads Christian Wrang Nielsen <madscwn@gmail.com>
2024-12-28 20:56:06 +0100

    fix: harpoon jump description

--- modules/home-manager/nvim/config/lua/plugins/harpoon.lua
+++ modules/home-manager/nvim/config/lua/plugins/harpoon.lua
@@ -11,8 +11,8 @@ return {
 		vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "harpoon: open harpoon" })
 
 		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "harpoon: go to 1" })
-		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "harpoon: go to 1" })
-		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "harpoon: go to 1" })
-		vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "harpoon: go to 1" })
+		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "harpoon: go to 2" })
+		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "harpoon: go to 3" })
+		vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "harpoon: go to 4" })
 	end,
 }
