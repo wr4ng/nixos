0f374df7a6a51c1c71fc1803840b218e5425a5a2
Mads Christian Wrang Nielsen <madscwn@gmail.com>
2024-12-28 20:44:13 +0100

    rename duplicate system packages

--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -135,13 +135,6 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-  ];
-
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
