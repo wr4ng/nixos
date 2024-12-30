f14d4e46cda8e21cc841d259665c9a92462687f8
Mads Christian Wrang Nielsen <madscwn@gmail.com>
2024-12-30 23:31:08 +0100

    fix: cannot use networkning.wireless and networking.networkmanager at the same time

--- hosts/yoga/configuration.nix
+++ hosts/yoga/configuration.nix
@@ -39,7 +39,7 @@
 	services.tlp.enable = true;
 
 	networking.hostName = "yoga"; # Define your hostname.
-	networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
 	# Enable networking
 	networking.networkmanager.enable = true;
 
