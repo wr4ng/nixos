6ebe03d8254301601101c3cbca62f15ac61071f1
Mads Christian Wrang Nielsen <madscwn@gmail.com>
2025-02-11 18:08:32 +0100

    update zellij config

--- modules/home-manager/zellij/default.nix
+++ modules/home-manager/zellij/default.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, config, ... }:
 
 {
-	programs.zellij.enable = true;
+	programs.zellij = {
+		enable = true;
+		enableZshIntegration = true;
+	};
 	home.file.".config/zellij".source = ./config;
 }
