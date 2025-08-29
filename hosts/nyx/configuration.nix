ac67343242924fccc881e46ed6e0e4e0ecc9ecf4
Mads Wrang <madscwn@gmail.com>
2025-08-29 10:49:29 +0200

    add localsend

--- hosts/nyx/configuration.nix
+++ hosts/nyx/configuration.nix
@@ -115,6 +115,11 @@
 
   programs.git.enable = true;
 
+  programs.localsend = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
