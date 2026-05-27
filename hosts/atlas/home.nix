097629a9e1c526fa4f52155dcf272037780eb6a6
Mads Wrang <madscwn@gmail.com>
2026-05-27 17:08:38 +0200

    atlas: disable mouse acceleration

--- hosts/atlas/home.nix
+++ hosts/atlas/home.nix
@@ -113,6 +113,9 @@
     "org/gnome/shell/extensions/dash-to-dock" = {
       shortcut = [ ];
     };
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
   };
 
   home.pointerCursor = {
