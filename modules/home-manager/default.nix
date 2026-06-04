1dafe88205c0234d19fcf979402653743c75b9ad
Mads Wrang <madscwn@gmail.com>
2026-06-04 12:14:03 +0200

    remove helix home-manager module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,14 +2,12 @@
   imports = [
     ./shell.nix
     ./kitty.nix
-    ./helix.nix
     ./zellij
     ./neovim
   ];
 
   module.shell.enable = lib.mkDefault true;
   module.kitty.enable = lib.mkDefault true;
-  # module.helix.enable = lib.mkDefault true;
   module.zellij.enable = lib.mkDefault true;
   module.neovim.enable = lib.mkDefault true;
 }
