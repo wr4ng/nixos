adbe4094c369cbf296c926218bdeeec262fddfcb
Mads Christian Wrang Nielsen <madscwn@gmail.com>
2026-09-03 21:13:16 +0200

    fix: flutter-dev module using hledger config as enable option by mistake

--- modules/home-manager/flutter-dev.nix
+++ modules/home-manager/flutter-dev.nix
@@ -8,7 +8,7 @@
 {
   options.modules.flutter-dev.enable = lib.mkEnableOption "enables flutter development module";
 
-  config = lib.mkIf config.modules.hledger.enable {
+  config = lib.mkIf config.modules.flutter-dev.enable {
     home.packages = with pkgs; [
       flutter
       android-studio
